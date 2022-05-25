08a709be91504d800d0272e32862e66a2e32e6d6
strawstack <rhshadowgreen@gmail.com>
2022-05-25 11:03:01 -0700

    2015 d2 part 1 and 2

--- src/y2015/d2/p1.rs
+++ src/y2015/d2/p1.rs
@@ -1,4 +1,25 @@
+use std::cmp::min;
+
 #[allow(dead_code)]
 pub fn main(input: &str) {
-    println!("p1.rs: {}", input.chars().count());
+    let lst: Vec<Vec<i32>> = input
+        .lines()
+        .map(|x| x.split('x').map(|y| y.parse().unwrap()).collect())
+        .collect();
+
+    let mut total: i32 = 0;
+
+    for rect in lst {
+        let l: i32 = rect[0];
+        let w: i32 = rect[1];
+        let h: i32 = rect[2];
+        let s1: i32 = l * w;
+        let s2: i32 = w * h;
+        let s3: i32 = h * l;
+        let side_area: i32 = 2 * s1 + 2 * s2 + 2 * s3;
+        let smallest = min(s1, min(s2, s3));
+        total += side_area + smallest;
+    }
+
+    println!("{}", total);
 }
--- src/y2015/d2/p2.rs
+++ src/y2015/d2/p2.rs
@@ -1,4 +1,25 @@
+use std::cmp::min;
+
 #[allow(dead_code)]
 pub fn main(input: &str) {
-    println!("p2.rs: {}", input.chars().count());
+    let lst: Vec<Vec<i32>> = input
+        .lines()
+        .map(|x| x.split('x').map(|y| y.parse().unwrap()).collect())
+        .collect();
+
+    let mut total: i32 = 0;
+
+    for rect in lst {
+        let l: i32 = rect[0];
+        let w: i32 = rect[1];
+        let h: i32 = rect[2];
+        let s1: i32 = 2 * (l + w);
+        let s2: i32 = 2 * (w + h);
+        let s3: i32 = 2 * (h + l);
+        let small_face_border = min(s1, min(s2, s3));
+        let volume = l * w * h;
+        total += small_face_border + volume;
+    }
+
+    println!("{}", total);
 }

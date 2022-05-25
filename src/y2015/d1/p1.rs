8e2f3e59ba526cea20f8a98d082f4c6c61e8b2a6
strawstack <rhshadowgreen@gmail.com>
2022-05-25 11:02:41 -0700

    day one p1

--- src/y2015/d1/p1.rs
+++ src/y2015/d1/p1.rs
@@ -1,5 +1,7 @@
 #[allow(dead_code)]
 pub fn main(input: &str) {
-    let ans: i32 = input.chars().fold(0, |acc, x| if x == '(' { acc + 1 } else { acc - 1 });
+    let ans: i32 = input
+        .chars()
+        .fold(0, |acc, x| if x == '(' { acc + 1 } else { acc - 1 });
     println!("{}", ans);
 }

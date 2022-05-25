a22cbf47d00751744eda058a7c046bc55f2da431
strawstack <rhshadowgreen@gmail.com>
2022-05-24 22:07:09 -0700

    Set up project structure

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod y2015;
+mod y2016;
+mod y2017;
+mod y2018;
+mod y2019;
+mod y2020;
+mod y2021;
+mod y2022;
+
+fn main() {
+
+    let s : &str = "input";
+
+    y2015::d1::p1::main(s); 
+    
+    println!("done.");
+}
---
+++ src/y2015/d4/p2.rs
@@ -0,0 +1,4 @@
+#[allow(dead_code)]
+pub fn main(input : &str) {
+    println!("p2.rs: {}", input.chars().count());
+}

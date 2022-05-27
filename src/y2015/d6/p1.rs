04678c1e0081104a9e58bf0fa976f97b491eac01
strawstack <rhshadowgreen@gmail.com>
2022-05-26 20:25:15 -0700

    2015 day 6 part 1 and 2

--- src/y2015/d6/p1.rs
+++ src/y2015/d6/p1.rs
@@ -1,4 +1,83 @@
+use regex::Regex;
+use std::collections::HashSet;
+
+#[derive(Debug)]
+enum Cmd {
+    Toggle,
+    On,
+    Off,
+}
+
+#[derive(Debug)]
+struct Point(i32, i32);
+
+#[derive(Debug)]
+struct Op {
+    cmd: Cmd,
+    from: Point,
+    to: Point,  
+}
+
+fn parse_line(re: &Regex, line: &str) -> Op {
+    let items: Vec<&str> = re.split(line).collect();
+    match items[0] {
+        "toggle" => {
+            Op {
+                cmd: Cmd::Toggle,
+                from: Point(items[1].parse().unwrap(), items[2].parse().unwrap()),
+                to: Point(items[4].parse().unwrap(), items[5].parse().unwrap()),
+            }
+        },
+        "turn" => {
+            match items[1] {
+                "on" => {
+                    Op {
+                        cmd: Cmd::On,
+                        from: Point(items[2].parse().unwrap(), items[3].parse().unwrap()),
+                        to: Point(items[5].parse().unwrap(), items[6].parse().unwrap()),
+                    }
+                },
+                "off" => {
+                    Op {
+                        cmd: Cmd::Off,
+                        from: Point(items[2].parse().unwrap(), items[3].parse().unwrap()),
+                        to: Point(items[5].parse().unwrap(), items[6].parse().unwrap()),
+                    }
+                },
+                _ => panic!("Invalid command type.")
+            }   
+        },
+        _ => panic!("Invalid command name.")
+    }
+}
+
 #[allow(dead_code)]
 pub fn main(input: &str) {
-    println!("p1.rs: {}", input.chars().count());
+    let mut grid: HashSet<(i32, i32)> = HashSet::new();
+    let re = Regex::new(r" |,").unwrap();
+    for line in input.lines() {
+        let op: Op = parse_line(&re, line);
+        for r in op.from.0..=op.to.0 {
+            for c in op.from.1..=op.to.1 {
+                match op.cmd {
+                    Cmd::Toggle => {
+                        if grid.contains(&(r, c)) {
+                            grid.remove(&(r, c));
+                        } else {
+                            grid.insert((r, c));
+                        }
+                    },
+                    Cmd::On => {
+                        grid.insert((r, c));
+                    },
+                    Cmd::Off => {
+                        grid.remove(&(r, c));
+                    }
+                }       
+            }
+        }
+    }
+    
+    println!("{}", grid.len());  
+
 }
--- src/y2015/d6/p2.rs
+++ src/y2015/d6/p2.rs
@@ -1,4 +1,90 @@
+use regex::Regex;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+enum Cmd {
+    Toggle,
+    On,
+    Off,
+}
+
+#[derive(Debug)]
+struct Point(i32, i32);
+
+#[derive(Debug)]
+struct Op {
+    cmd: Cmd,
+    from: Point,
+    to: Point,  
+}
+
+fn parse_line(re: &Regex, line: &str) -> Op {
+    let items: Vec<&str> = re.split(line).collect();
+    match items[0] {
+        "toggle" => {
+            Op {
+                cmd: Cmd::Toggle,
+                from: Point(items[1].parse().unwrap(), items[2].parse().unwrap()),
+                to: Point(items[4].parse().unwrap(), items[5].parse().unwrap()),
+            }
+        },
+        "turn" => {
+            match items[1] {
+                "on" => {
+                    Op {
+                        cmd: Cmd::On,
+                        from: Point(items[2].parse().unwrap(), items[3].parse().unwrap()),
+                        to: Point(items[5].parse().unwrap(), items[6].parse().unwrap()),
+                    }
+                },
+                "off" => {
+                    Op {
+                        cmd: Cmd::Off,
+                        from: Point(items[2].parse().unwrap(), items[3].parse().unwrap()),
+                        to: Point(items[5].parse().unwrap(), items[6].parse().unwrap()),
+                    }
+                },
+                _ => panic!("Invalid command type.")
+            }   
+        },
+        _ => panic!("Invalid command name.")
+    }
+}
+
 #[allow(dead_code)]
 pub fn main(input: &str) {
-    println!("p2.rs: {}", input.chars().count());
+    let mut grid: HashMap<(i32, i32), i32> = HashMap::new();
+    let re = Regex::new(r" |,").unwrap();
+    for line in input.lines() {
+        let op: Op = parse_line(&re, line);
+        for r in op.from.0..=op.to.0 {
+            for c in op.from.1..=op.to.1 {
+                match op.cmd {
+                    Cmd::Toggle => {
+                        let value = grid.entry((r, c)).or_insert(0);
+                        *value += 2;
+                    },
+                    Cmd::On => {
+                        let value = grid.entry((r, c)).or_insert(0);
+                        *value += 1;
+                    },
+                    Cmd::Off => {
+                        let value = grid.entry((r, c)).or_insert(0);
+                        *value -= 1;
+                        if *value < 0 {
+                            *value = 0;
+                        }
+                    }
+                }       
+            }
+        }
+    }
+    
+
+    let mut total: i32 = 0;
+    for (_, bright) in grid {
+        total += bright;
+    }
+    
+    println!("{}", total);
 }
